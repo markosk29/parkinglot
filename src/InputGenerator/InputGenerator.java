292d02efcc2993fcf905f5bb7a5fe695f505c701
CioleaFlorin <ciolea.florin.99@gmail.com>
2021-07-29 06:19:47 -0700

    updated random plates

--- src/InputGenerator/InputGenerator.java
+++ src/InputGenerator/InputGenerator.java
@@ -72,4 +72,24 @@ public class InputGenerator {
     public static void main (String[] args) throws ClassNotFoundException, InvocationTargetException, InstantiationException, IllegalAccessException {
         InputGenerator.createParkingLot().summary();
     }
+
+    public  String createRandomPlate() {
+        String generatedString2 = generate(2, 48, 57);
+        String generatedString = generate(2, 65, 90);
+        String generatedString3 = generate(3, 65, 90);
+
+        return generatedString + generatedString2 + generatedString3;
+    }
+
+
+    private  String generate(int targetStringLength, int leftRange, int rightRange) {
+        Random random = new Random();
+        StringBuilder buffer = new StringBuilder(targetStringLength);
+        for (int i = 0; i < targetStringLength; i++) {
+            int randomLimitedInt = leftRange + (int)
+                    (random.nextFloat() * (rightRange - leftRange + 1));
+            buffer.append((char) randomLimitedInt);
+        }
+        return buffer.toString();
+    }
 }
