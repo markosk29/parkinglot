b305dac8d4fdde56d2ed9810bc926b8736b7e1df
Stefan Uifalean <stefan.uifalean@avangarde-software.com>
2021-07-30 11:42:39 +0300

    Updated IFloorBuilder and created createParkingLot method.

--- src/InputGenerator/IFloorBuilderImpl.java
+++ src/InputGenerator/IFloorBuilderImpl.java
@@ -8,16 +8,40 @@ import com.avangarde.parkinglot.parking.services.ParkingLot;
 import java.lang.reflect.InvocationTargetException;
 import java.util.*;
 
-public class InputGenerator implements IFloorBuilder {
+public class IFloorBuilderImpl implements IFloorBuilder {
     private static final int MIN_FLOORS = 2;
     private static final int MAX_FLOORS = 10;
     private static Random random = new Random();
 
+    @Override
+    public ParkingFloor createParkingFloor(int floorNo, List<ParkingSpotLotSize> parkingSpotLotSizes) {
+        ParkingSpotFactory parkingSpotFactory = new ParkingSpotFactory();
+        Map<SpotType, List<ParkingSpot>> spotPairs = new HashMap<>();
+        for(var lot : parkingSpotLotSizes) {
+            List<ParkingSpot> parkingSpots = new ArrayList<>();
+            for(int spotCount = 0; spotCount < lot.getSize(); spotCount++) {
+                // adding parking spots to the parking floor
+                parkingSpots.add(parkingSpotFactory.createParkingSpot(lot.getType()));
+            }
+            if (! spotPairs.containsKey(lot.getType())) {
+                spotPairs.put(lot.getType(), parkingSpots);
+            }
+            else {
+                var newList = spotPairs.get(lot.getType());
+                newList.addAll(parkingSpots);
+                spotPairs.put(lot.getType(), newList);
+            }
+        }
+        var floor = ParkingFloor.ParkingFloorBuilder.builder().floorID(floorNo).spotPairs(spotPairs).build();
+        return floor;
+    }
+
     /**
+     * TODO: Extract method in a separate interface
      * Returns a randomly configured parking lot with all spots free
      * @return parking lot
      */
-    public ParkingLot createParkingLot() throws ClassNotFoundException, InvocationTargetException, InstantiationException, IllegalAccessException {
+    public ParkingLot createParkingLot() {
         var noOfFloors = rand(MIN_FLOORS, MAX_FLOORS); // inclusive
         var parkingLot = new ParkingLot();
         for (int floorCount = 0; floorCount < noOfFloors; floorCount++) {
@@ -28,13 +52,17 @@ public class InputGenerator implements IFloorBuilder {
 
     // HELPER METHODS
 
+    /**
+     * Randomly chooses between 0 and 1; We use it to decide if we create a particular spot type in a floor
+     * @return boolean
+     */
     private static boolean acceptOrDecline() {
         if ( rand(0, 1) == 0) {
             return false;
         }
         return true;
-
     }
+
     private List<ParkingSpotLotSize> generateRandomSpotLots() {
         List<ParkingSpotLotSize> spotsLots = new ArrayList<>();
         for (var type : SpotType.values()) { // create spot types
@@ -50,26 +78,5 @@ public class InputGenerator implements IFloorBuilder {
 
     private static int rand (int min, int max) {
        return (int) (Math.floor((Math.random() * (max-min+1) + min)));
-    }
-
-    public static void main (String[] args) {
-//        InputGenerator.createParkingLot().summary();
-    }
-
-    @Override
-    public ParkingFloor createParkingFloor(int floorNo, List<ParkingSpotLotSize> parkingSpotLotSizes) {
-        ParkingSpotFactory parkingSpotFactory = new ParkingSpotFactory();
-        Map<SpotType, List<ParkingSpot>> spotPairs = new HashMap<>();
-        for(var lot : parkingSpotLotSizes) {
-            List<ParkingSpot> parkingSpots = new ArrayList<>();
-            for(int spotCount = 0; spotCount < lot.getSize(); spotCount++) {
-                // adding parking spots to the parking floor
-                parkingSpots.add(parkingSpotFactory.createParkingSpot(lot.getType()));
-            }
-            // Breaks if given duplicate types of ParkingSpotLotSize type one after another
-            spotPairs.put(lot.getType(), parkingSpots);
-        }
-        var floor = ParkingFloor.ParkingFloorBuilder.builder().floorID(floorNo).spotPairs(spotPairs).build();
-        return floor;
     }
 }
--- src/com/avangarde/parkinglot/parking/models/HandicapSpot.java
+++ src/com/avangarde/parkinglot/parking/models/HandicapSpot.java
@@ -1,7 +1,5 @@
 package com.avangarde.parkinglot.parking.models;
 
-import com.avangarde.parkinglot.vehicle.models.Vehicle;
-
 public class HandicapSpot extends ParkingSpot {
 
     public HandicapSpot() {
