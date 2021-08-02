d96cf6f30a480b20763aa20c08af5f79a588b6b8
Stefan Uifalean <stefan.uifalean@avangarde-software.com>
2021-08-02 12:10:27 +0300

    Relativised file input path and updated .gitignore

--- src/com/avangarde/Main.java
+++ src/com/avangarde/Main.java
@@ -1,12 +1,7 @@
 package com.avangarde;
 
-import com.avangarde.parkinglot.parking.models.ParkingSpot;
-import com.avangarde.parkinglot.parking.models.SpotType;
 import com.avangarde.parkinglot.parking.services.ParkingLot;
 import com.avangarde.parkinglot.utils.InputFileImpl;
-
-import java.util.List;
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
