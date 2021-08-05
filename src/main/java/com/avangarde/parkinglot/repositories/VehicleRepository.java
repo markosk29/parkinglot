1d44b20e2251b049812fdc7d1bc211003c52e304
markosk29 <markos.kosa@avangarde-software.com>
2021-08-05 17:41:12 +0300

    modified loadVehicles method

--- src/main/java/com/avangarde/parkinglot/repositories/VehicleRepository.java
+++ src/main/java/com/avangarde/parkinglot/repositories/VehicleRepository.java
@@ -1,10 +1,11 @@
 package com.avangarde.parkinglot.repositories;
 
+import com.avangarde.parkinglot.parking.services.ParkingLot;
 import com.avangarde.parkinglot.vehicle.models.Vehicle;
 
 import java.util.List;
 
 public interface VehicleRepository {
-    List<Vehicle> loadAllVehicles();
+    List<Vehicle> loadLatestVehicles(ParkingLot parkingLot);
     boolean updateParkingSpot(int id, boolean isOccupied);
 }
--- src/main/java/com/avangarde/parkinglot/repositories/VehicleRepositoryImpl.java
+++ src/main/java/com/avangarde/parkinglot/repositories/VehicleRepositoryImpl.java
@@ -1,13 +1,23 @@
 package com.avangarde.parkinglot.repositories;
 
+import com.avangarde.parkinglot.parking.models.SpotType;
+import com.avangarde.parkinglot.parking.services.ParkingFloor;
+import com.avangarde.parkinglot.parking.services.ParkingLot;
 import com.avangarde.parkinglot.utils.DBUtil;
+import com.avangarde.parkinglot.utils.InputFileImpl;
 import com.avangarde.parkinglot.vehicle.models.Vehicle;
 import com.avangarde.parkinglot.vehicle.services.VehicleBuilder;
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+/**
+ * TODO:
+ *  - TEST loadLatestVehicles method
+ */
 public class VehicleRepositoryImpl implements VehicleRepository {
 
     private List<Vehicle> vehicles;
@@ -17,18 +27,37 @@ public class VehicleRepositoryImpl implements VehicleRepository {
     }
 
     @Override
-    public List<Vehicle> loadAllVehicles() {
+    public List<Vehicle> loadLatestVehicles(ParkingLot parkingLot) {
         DBUtil dbUtil = new DBUtil();
         dbUtil.open();
 
-        String query = "SELECT * FROM parking.vehicles";
+        Map<String, Integer> freeSpots = getNumberOfVehicles(parkingLot);
+
+        String query = "SELECT * FROM parking.vehicles WHERE vehicle_type IN (";
+
+        for(var set : freeSpots.entrySet()) {
+            query += "'" +set.getKey()+ "',";
+
+        }
+
+        query = query.substring(0, query.length() - 1);
+        query += ") ORDER BY id DESC;";
+
+        System.out.println(query);
 
         try(Statement statement = dbUtil.getConn().createStatement();
             ResultSet resultSet = statement.executeQuery(query)){
 
             while(resultSet.next()) {
-                vehicles.add(VehicleBuilder.builder()
-                        .createVehicle(resultSet.getString("vehicle_type"), resultSet.getString("plate")));
+                if(freeSpots.containsKey(resultSet.getString("vehicle_type")) &&
+                        freeSpots.get(resultSet.getString("vehicle_type")) > 0) {
+
+                    vehicles.add(VehicleBuilder.builder()
+                            .createVehicle(resultSet.getString("vehicle_type"), resultSet.getString("plate")));
+
+                    freeSpots.replace(resultSet.getString("vehicle_type"),
+                            freeSpots.get(resultSet.getString("vehicle_type")) - 1);
+                }
             }
 
         } catch(SQLException e) {
@@ -65,5 +94,32 @@ public class VehicleRepositoryImpl implements VehicleRepository {
         for(Vehicle vehicle : this.vehicles){
             System.out.println(vehicle.getInfo());
         }
+    }
+
+    private Map<String, Integer> getNumberOfVehicles(ParkingLot parkingLot) {
+        Map<String, Integer> freeSpots = new HashMap<>();
+        List<ParkingFloor> parkingFloors = parkingLot.getFloors();
+
+        for(ParkingFloor parkingFloor : parkingFloors) {
+            for (var entry : parkingFloor.getSpotPairs().entrySet()) {
+                int count = 0;
+
+                for (var spot : entry.getValue()) {
+                    if (!spot.isOcuppied()) {
+                        count++;
+                    }
+                }
+
+                freeSpots.put(String.valueOf(entry.getKey()), count);
+            }
+        }
+
+        return freeSpots;
+    }
+
+    public static void main(String[] args) {
+        InputFileImpl inputFile = new InputFileImpl();
+        VehicleRepositoryImpl vehicleRepository = new VehicleRepositoryImpl();
+        vehicleRepository.loadLatestVehicles(inputFile.readParkingLot("C:\\Users\\Kosa Markos\\IdeaProjects\\parkinglot\\src\\main\\resources\\input.txt"));
     }
 }
