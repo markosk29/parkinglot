3f301642021950258d8bb42dea1ef17bbcc37ca2
CioleaFlorin <ciolea.florin.99@gmail.com>
2021-08-06 12:46:01 +0300

    added unpark class with unpark vehicle from db functionality

--- src/main/java/com/avangarde/parkinglot/ParkinglotApplication.java
+++ src/main/java/com/avangarde/parkinglot/ParkinglotApplication.java
@@ -1,9 +1,26 @@
 package com.avangarde.parkinglot;
 
+import com.avangarde.parkinglot.utils.DBUtil;
+
+import java.sql.SQLException;
+
 public class ParkinglotApplication {
 
-	public static void main(String[] args) {
-		System.out.println("Functioneaza!");
+
+
+
+
+	public static void main(String[] args) throws SQLException {
+
+		UnPark unPark = new UnPark();
+		unPark.unparkRandomVehicles();
+
 	}
+
+
+
+
+
+
 
 }
