4fb68e02000333f373da77db2d96302944b992c3
Stefan Uifalean <stefan.uifalean@avangarde-software.com>
2021-08-10 13:36:17 +0300

    Updated transaction to using entirely jdbc

--- src/main/java/TransactionsLesson.java
+++ src/main/java/TransactionsLesson.java
@@ -1,27 +1,42 @@
 import com.avangarde.parkinglot.utils.DBUtil;
 
 import java.sql.SQLException;
+import java.sql.Savepoint;
 import java.sql.Statement;
 
 public class TransactionsLesson {
     public static void main(String[] args) throws SQLException {
-        String sql = "BEGIN;" +
-                "SAVEPOINT sp0;" +
-                "INSERT INTO parking.vehicles VALUES (DEFAULT, 'CAR', 'B79PUP');" +
-                "SAVEPOINT sp1;" +
-                "INSERT INTO parking.vehicles VALUES (DEFAULT, 'CAR', 'B79PUP');" +
-                "INSERT INTO parking.vehicles VALUES (DEFAULT, 'CAR', 'CJ34AVG');";
+        new TransactionsLesson().jdbc();
+    }
+
+    private void jdbc() throws SQLException {
         DBUtil dbUtil = new DBUtil();
+        Savepoint initialSavePoint = null;
+        Savepoint afterInsertSavePoint = null;
         dbUtil.open();
-        Statement statement = dbUtil.getConn().createStatement();
+
+        String sql1 = "INSERT INTO parking.vehicles VALUES (DEFAULT, 'CAR', 'B79PUP');";
+        String sql2 = "INSERT INTO parking.vehicles VALUES (DEFAULT, 'CAR', 'B79PUP');";
+        String sql3 = "INSERT INTO parking.vehicles VALUES (DEFAULT, 'CAR', 'CJ34AVG');";
         try {
-            statement.executeUpdate(sql);
-        } catch (SQLException e) {
-            sql = "ROLLBACK TO sp1; COMMIT;";
-            statement.executeUpdate(sql);
-        } finally {
+            dbUtil.getConn().setAutoCommit(false);
+
+            Statement statement = dbUtil.getConn().createStatement();
+            initialSavePoint = dbUtil.getConn().setSavepoint();
+            statement.executeUpdate(sql1);
+            afterInsertSavePoint = dbUtil.getConn().setSavepoint();
+            statement.executeUpdate(sql2);
+            statement.executeUpdate(sql3);
+            statement.getConnection().commit();
+
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            dbUtil.getConn().rollback(initialSavePoint);
+
+        }
+        finally {
             dbUtil.close();
-            statement.close();
         }
     }
 }
